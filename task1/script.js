f1ac5596e133145832ba9d183770f6683e7e0da0
Yashwanth K <yashwanth.kuppuraj05@gmail.com>
2025-06-17 09:00:41 +0530

    Initial Commit

---
+++ task1/script.js
@@ -0,0 +1,8 @@
+function showPopup() {
+    document.getElementById("popup").classList.add("show");
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").classList.remove("show");
+  }
+  
